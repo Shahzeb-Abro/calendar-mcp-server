8c2b1987b3d90dd3ff3aa1fec29a0900123a626e
shahzeb.abro <shahzeb.abro@adalfi.com>
2026-04-07 14:47:16 +0500

    Added credentials and token directory path in application.properties for easier editing

--- src/main/java/com/shahzeb/calendarmcp/config/GoogleCalendarConfig.java
+++ src/main/java/com/shahzeb/calendarmcp/config/GoogleCalendarConfig.java
@@ -11,21 +11,23 @@ import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.calendar.Calendar;
 import com.google.api.services.calendar.CalendarScopes;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 
 import java.io.File;
 import java.io.FileReader;
-import java.io.InputStreamReader;
 import java.util.List;
 
 @Configuration
 public class GoogleCalendarConfig {
+
     private static final List<String> SCOPES = List.of(CalendarScopes.CALENDAR);
-    private static final String PROJECT_DIR = "C:/Users/Shahzeb/calendar-mcp";
-    private static final String CREDENTIALS_PATH = PROJECT_DIR + "/src/main/resources/credentials.json";
-    private static final String TOKENS_DIR = PROJECT_DIR + "/tokens";
+    @Value("${app.credentials.path}")
+    private String credentialsPath;
+
+    @Value("${app.tokens.dir}")
+    private String tokensDir;
 
     @Bean
     public Calendar googleCalendarService() throws Exception {
@@ -35,14 +37,14 @@ public class GoogleCalendarConfig {
 
         GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(
                 jsonFactory,
-                new FileReader(CREDENTIALS_PATH)
+                new FileReader(credentialsPath)
         );
 
 
         GoogleAuthorizationCodeFlow flow  = new GoogleAuthorizationCodeFlow.Builder(
                 transport, jsonFactory, clientSecrets, SCOPES
         )
-                .setDataStoreFactory(new FileDataStoreFactory(new File(TOKENS_DIR)))
+                .setDataStoreFactory(new FileDataStoreFactory(new File(tokensDir)))
                 .setAccessType("offline")
                 .build();
 
